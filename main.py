843a53a66b0aec5e73c3e1fc6e9c1320378797e8
Pedro Westin <102073492+PedroWestin@users.noreply.github.com>
2026-03-12 17:36:03 -0300

    Update main.py
    adding the option to filter user emails by  sex

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ def filter_age(age=None):
     data_filtered = data_frame[data_frame['Idade'] <= age]
     return data_filtered['Email']
         
+def filter_sex(sex=None):
+    data_filtered = data_frame[data_frame['Sexo'] == sex]
+    return data_filtered['Email']
+
 print(data_frame)
 print('-=' * 50)
 print(filter_age(30)) 
+print('-=' * 50)
+print(filter_sex('Masculino'))
